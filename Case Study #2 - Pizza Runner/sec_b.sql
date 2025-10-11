295b521cd387c346cea66dd0b15097c4d755bd43
IMRAN <imran.ali.96@hotmail.com>
2025-10-11 03:02:57 +0100

    Q2

--- Case Study #2 - Pizza Runner/sec_b.sql
+++ Case Study #2 - Pizza Runner/sec_b.sql
@@ -3,17 +3,27 @@
 -- 1.How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
 
 SELECT 
-DATE_TRUNC('week', r."registration_date") + 4  as "week",
-COUNT(r."runner_id") as runners_signed_up
+    DATE_TRUNC('week', r."registration_date") + 4  as "week",
+    COUNT(r."runner_id") as runners_signed_up
 FROM runners as r
 GROUP BY DATE_TRUNC('week', r."registration_date") + 4
 ORDER BY DATE_TRUNC('week', r."registration_date") + 4;
 
+-- 2.What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+
+SELECT 
+    ro."runner_id",
+    ROUND(AVG(TIMEDIFF('minute',co."order_time",ro."pickup_time"::timestamp_ntz)),0) as AVG_TIME
+FROM customer_orders as co 
+LEFT JOIN runner_orders as ro USING("order_id")
+WHERE ro."pickup_time" <> 'null'
+GROUP BY ro."runner_id"
+ORDER BY ro."runner_id";
+
+-- 3.Is there any relationship between the number of pizzas and how long the order takes to prepare?
 
 
 
--- What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
--- Is there any relationship between the number of pizzas and how long the order takes to prepare?
 -- What was the average distance travelled for each customer?
 -- What was the difference between the longest and shortest delivery times for all orders?
 -- What was the average speed for each runner for each delivery and do you notice any trend for these values?
